e8d0d3670fe43a89db125f069fa2bf21dbbb00ee
Ian Scott <chesnekov@gmail.com>
2020-06-05 11:42:34 -0700

    resolve logic errors in branching that resulted in non-expected result

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ $("document").ready(function() {
   $("form#programmingQuiz").submit(function(event) {
     event.preventDefault();
 
-    const ophidiophobiaStatus = $("input:radio[name=ophidiophobia]:checked").val();
-    const rigidityValue = ($("input:radio[name=rigidity]:checked").val());
+    const ophidiophobiaStatus = JSON.parse($("input:radio[name=ophidiophobia]:checked").val());
+    const rigidityValue = parseInt(($("input:radio[name=rigidity]:checked").val()));
     const airSpeedResponse = $("input#airSpeed").val();
     const favColor = $("#color").val();
     const queensResponse = parseInt($("#queens").val());
